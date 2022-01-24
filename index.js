a6428402564e9b99f0dd790a774646f1eb6568a7
Stella <jens300304@gmail.com>
2022-01-24 16:20:21 +0200

    whoops, wasn't word matching + use " instead of '

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const { Plugin } = require('powercord/entities');
-const { inject, uninject } = require('powercord/injector');
-const { getModule } = require('powercord/webpack');
-const Settings = require('./settings.jsx');
+const { Plugin } = require("powercord/entities");
+const { inject, uninject } = require("powercord/injector");
+const { getModule } = require("powercord/webpack");
+const Settings = require("./settings.jsx");
 
 module.exports = class SlurNuker extends Plugin {
   async startPlugin() {
-    const {default: MessageRender} = await getModule(['getElementFromMessageId']);
-    inject('replace-bad', MessageRender, 'type', (args) => {
+    const {default: MessageRender} = await getModule(["getElementFromMessageId"]);
+    inject("replace-bad", MessageRender, "type", (args) => {
       const [{message}] = args;
       if (!message || !message.content) return args;
-      for (const bad of this.settings.get('badWords', [])) {
-        if (bad == undefined || bad == '') continue;
-        let regex = new RegExp(bad, 'gim');
-        message.content = message.content.replace(regex, '[slur]');
+      for (const bad of this.settings.get("badWords", [])) {
+        if (bad == undefined || bad == "") continue;
+        let regex = new RegExp(`\\b(${bad})`, "gim");
+        message.content = message.content.replace(regex, "[slur]");
       }
       return args;
     }, true);
 
-    powercord.api.settings.registerSettings('slur-nuker', {
+    powercord.api.settings.registerSettings("slur-nuker", {
       category: this.entityID,
-      label: 'Slur Nuker',
+      label: "Slur Nuker",
       render: Settings
     });
   }
   async pluginWillUnload() {
-    uninject('replace-bad');
-    powercord.api.settings.unregisterSettings('slur-nuker');
+    uninject("replace-bad");
+    powercord.api.settings.unregisterSettings("slur-nuker");
   }
 };
